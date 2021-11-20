a441166ec1a57f62dc8617e4ba998e433723ec67
siddhN1811 <94180972+siddhN1811@users.noreply.github.com>
2021-11-20 23:57:56 +0530

    comments

--- javaMiniProject/src/javaGui.java
+++ javaMiniProject/src/javaGui.java
@@ -16,37 +16,42 @@ public class javaGui extends JFrame{
     private JButton hintButton;
     private JLabel Label;
 
+
     public javaGui() throws FileNotFoundException {
 
         File file = new File("E:\\JAVA-Mini-Project\\javaMiniProject\\words.txt");
         Scanner sc = new Scanner(file);
 
         String t = sc.nextLine ();//text
-        String th = sc.nextLine();//text hint
-        String t2 = t; //
+        String td = sc.nextLine();//text description
+        String t2 = t;
+
         t2 = t2.replaceAll("\\w", String.valueOf('-'));  //printing dashes
+
         Label.setText(t2); // modifying dashes according to the word
         //System.out.println();
+
+
         submitButton.addActionListener(new ActionListener() {
             @Override
 
             public void actionPerformed(ActionEvent e) {
 
                 String t1 = textField1.getText();
-
+                //correct word
                 if (CharSequence.compare(t, t1) == 0){
                         JOptionPane.showMessageDialog(null, "It's the correct word !!");
                 }
-                else{
+                else{//incorrect guess
                     JOptionPane.showMessageDialog(null, "incorrect guess");
                 }
             }
         });
         hintButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) {//description button
 
-                JOptionPane.showMessageDialog(null, th);
+                JOptionPane.showMessageDialog(null, td);
             }
         });
 
@@ -55,6 +60,7 @@ public class javaGui extends JFrame{
     }
 
     public static void main(String[] args) throws FileNotFoundException {
+
         javaGui j = new javaGui();
         j.setContentPane(new javaGui().panel1);
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
